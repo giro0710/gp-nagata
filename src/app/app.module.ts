f060b3cc2672abf2672c8db6b4a071effbe86978
Tahori <girocalvario@gmail.com>
2020-04-07 18:59:04 +0800

    Add happening page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,24 @@ import { AboutUsComponent } from "./about-us/about-us.component";
 import { ProductAndServicesComponent } from "./product-and-services/product-and-services.component";
 import { ContactUsComponent } from "./contact-us/contact-us.component";
 import { HappeningsComponent } from "./happenings/happenings.component";
+import { HappeningComponent } from "./happening/happening.component";
 
 const appRoutes: Routes = [
   { path: "home", component: HomeComponent },
   {
     path: "about",
     component: AboutUsComponent,
-    data: { title: "Heroes List" }
+    data: { title: "Heroes List" },
   },
   { path: "projects", component: ProductAndServicesComponent },
   { path: "happenings", component: HappeningsComponent },
+  {
+    path: "happenings/The Start of the GP-Nagata Collaboration",
+    component: HappeningComponent,
+  },
   { path: "connect", component: ContactUsComponent },
   { path: "", redirectTo: "/home", pathMatch: "full" },
-  { path: "**", redirectTo: "/home" }
+  { path: "**", redirectTo: "/home" },
 ];
 
 @NgModule({
@@ -31,10 +36,11 @@ const appRoutes: Routes = [
     AboutUsComponent,
     ProductAndServicesComponent,
     ContactUsComponent,
-    HappeningsComponent
+    HappeningsComponent,
+    HappeningComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
